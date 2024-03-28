8c31b75461ad29dd33b09e7d90484c7a114c1b30
benj-codes <ben-eng@outlook.com>
2024-03-28 00:23:51 +0000

    Updated_Models&Views

--- datasets/admin.py
+++ datasets/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Dataset
+from .models import Dataset, Review, Tag
 
 # Register your models here.
 admin.site.register(Dataset)
+admin.site.register(Review)
+admin.site.register(Tag)
--- datasets/views.py
+++ datasets/views.py
@@ -1,34 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-datasetsList = [
-    {
-        'id': '1',
-        'title': 'Japan Birth Demographics',
-        'description': 'Data analysis applied to Japan Birth Demographics dataset.'
-    },
-    {
-        'id': '2',
-        'title': 'Crime Statistics Calgary',
-        'description': 'Data analysis applied to Crime Statistics Calgary dataset.'
-    },
-    {
-        'id': '3',
-        'title': 'Video game sales',
-        'description': 'Data analysis applied to Video Game Sales dataset.'
-    },
-]
+from .models import Dataset
 
 # Create your views here.
 def datasets(request):
-    page = 'Datasets'
-    number = 10
-    context = {'page': page, 'number':number, 'datasets': datasetsList}
+    datasets = Dataset.objects.all()
+    context = {'datasets': datasets}
     return render(request, 'datasets/datasets.html', context)
 
 def dataset(request, pk):
-    datasetObj = None
-    for i in datasetsList:
-        if i['id'] == pk:
-            datasetObj = i
+    datasetObj = Dataset.objects.get(id_num=pk)
     return render(request, 'datasets/single-dataset.html', {'dataset':datasetObj})
